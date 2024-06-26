fd9c814b1c5673da94ea4b64586f280147bcc2b1
crouch86 <81594192+crouch86@users.noreply.github.com>
2024-06-26 16:47:52 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_a24.mk
 
