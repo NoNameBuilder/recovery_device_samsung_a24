518d7207e1adbb902b804c65e57d0f3619db7e70
NoNameBuilder <163744362+NoNameBuilder@users.noreply.github.com>
2024-08-10 21:36:52 +0300

    PBRP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_a24.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/pb_a24.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a24-user \
-    twrp_a24-userdebug \
-    twrp_a24-eng
+    pb_a24-user \
+    pb_a24-userdebug \
+    pb_a24-eng
